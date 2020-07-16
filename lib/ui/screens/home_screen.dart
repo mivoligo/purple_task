21beac5d4205ec44668800f2a29093e919dffc2c
Michal Predotka <mivoligo@gmail.com>
2020-07-16 21:55:10 +0100

    Use ListView to display categories

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:to_do/ui/widgets/category_card.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double _appWidth;
+  double _appHeight;
+
+  bool get _isPortrait => _appWidth < _appHeight;
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        child: CategoryCard(
-          isEmpty: true,
-          categoryName: 'Work',
+    _appWidth = MediaQuery.of(context).size.width;
+    _appHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Flex(
+          direction: _isPortrait ? Axis.vertical : Axis.horizontal,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Text('Monday 25 September'),
+            ),
+            Expanded(
+              flex: 6,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context, index) => CategoryCard(
+                  categoryName: 'index: $index',
+                  isEmpty: index == 3 ? true : false,
+                  paddingLeft: index == 0 ? 32.0 : 16.0,
+                  paddingRight: index == 3 ? 32.0 : 16.0,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/ui/widgets/category_card.dart
+++ lib/ui/widgets/category_card.dart
@@ -3,30 +3,43 @@ import 'package:flutter/material.dart';
 class CategoryCard extends StatelessWidget {
   final bool isEmpty;
   final String categoryName;
+  final double paddingLeft;
+  final double paddingRight;
 
-  const CategoryCard({Key key, this.categoryName, this.isEmpty})
-      : super(key: key);
+  const CategoryCard({
+    Key key,
+    this.categoryName,
+    this.isEmpty,
+    this.paddingLeft = 16.0,
+    this.paddingRight = 16.0,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: isEmpty
-            ? [
-                Text('+'),
-                Text('Add Category'),
-              ]
-            : [
-                Text(categoryName),
-                Container(
-                  height: 200,
-                  color: Colors.blue,
-                ),
-                Divider(
-                  thickness: 2.0,
-                ),
-                Text('Add Task'),
-              ],
+    return Padding(
+      padding: EdgeInsets.fromLTRB(paddingLeft, 16.0, paddingRight, 32.0),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Card(
+          child: Column(
+            children: isEmpty
+                ? [
+                    Text('+'),
+                    Text('Add Category'),
+                  ]
+                : [
+                    Text(categoryName),
+                    Container(
+                      height: 200,
+                      color: Colors.green,
+                    ),
+                    Divider(
+                      thickness: 2.0,
+                    ),
+                    Text('Add Task'),
+                  ],
+          ),
+        ),
       ),
     );
   }
