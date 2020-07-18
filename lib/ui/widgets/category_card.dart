13ab89ee53d1ed4ed8c6cf3d891cedf5851c563f
Michal Predotka <mivoligo@gmail.com>
2020-07-18 20:53:14 +0100

    Remove unused code

--- lib/ui/widgets/category_card.dart
+++ lib/ui/widgets/category_card.dart
@@ -31,7 +31,6 @@ class CategoryCard extends StatelessWidget {
             child: InkWell(
               onTap: () {},
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(16.0),
