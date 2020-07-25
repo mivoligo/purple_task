59d74596ed9fadb7df15a1eeba8b4c9fa7eabb4e
Michal Predotka <mivoligo@gmail.com>
2020-07-25 13:14:28 +0100

    Add list of colors

---
+++ lib/ui/view_models/category_colors.dart
@@ -0,0 +1,23 @@
+class CategoryColors {
+  List<int> categoryColors = [
+    0xfff44336,
+    0xffe91e63,
+    0xff9c27b0,
+    0xff673ab7,
+    0xff3f51b5,
+    0xff2196f3,
+    0xff03a9f4,
+    0xff00bcd4,
+    0xff009688,
+    0xff4caf50,
+    0xff8bc34a,
+    0xffcddc39,
+    0xffffeb3b,
+    0xffffc107,
+    0xffff9800,
+    0xffff5722,
+    0xff795548,
+    0xff9e9e9e,
+    0xff607d8b,
+  ];
+}
