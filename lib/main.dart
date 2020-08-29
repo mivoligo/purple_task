aef2306f9d7da9b2865de6fad003ea742a7c0dfd
Michal Predotka <mivoligo@gmail.com>
2020-08-29 10:54:50 +0100

    Remove focus from text fields when clicking outside of them

--- lib/main.dart
+++ lib/main.dart
@@ -39,74 +39,83 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: APP_NAME,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Color(0xffe83f6f),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
+    // remove focus from text field when clicking outside
+    return Listener(
+      onPointerDown: (_) {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          currentFocus.focusedChild.unfocus();
+        }
+      },
+      child: MaterialApp(
+        title: APP_NAME,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Color(0xffe83f6f),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          buttonBarTheme: ButtonBarThemeData(
+            alignment: MainAxisAlignment.spaceBetween,
+          ),
+          textTheme: TextTheme(
+              // category name
+              headline4: TextStyle(
+                color: Colors.black,
+                fontSize: 24.0,
+                fontWeight: FontWeight.w400,
+              ),
+              // hello
+              headline5: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.w300,
+              ),
+              // greetings info
+              headline6: TextStyle(
+                color: Colors.white70,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w300,
+              ),
+              // task name,
+              // info with number of tasks left for category
+              subtitle1: TextStyle(
+                color: Colors.black,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400,
+              ),
+              // task name when done
+              subtitle2: TextStyle(
+                color: Colors.grey,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w300,
+                decoration: TextDecoration.lineThrough,
+              ),
+              // dialog title when adding new category
+              bodyText1: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 18.0,
+                fontWeight: FontWeight.w400,
+              ),
+              // info text when adding new category
+              bodyText2: TextStyle(
+                color: Colors.grey,
+                fontSize: 14.0,
+              ),
+              button: TextStyle(
+                color: Colors.white,
+                fontSize: 14.0,
+                fontWeight: FontWeight.w400,
+                letterSpacing: 0.8,
+              )),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        buttonBarTheme: ButtonBarThemeData(
-          alignment: MainAxisAlignment.spaceBetween,
-        ),
-        textTheme: TextTheme(
-            // category name
-            headline4: TextStyle(
-              color: Colors.black,
-              fontSize: 24.0,
-              fontWeight: FontWeight.w400,
-            ),
-            // hello
-            headline5: TextStyle(
-              color: Colors.white,
-              fontSize: 24.0,
-              fontWeight: FontWeight.w300,
-            ),
-            // greetings info
-            headline6: TextStyle(
-              color: Colors.white70,
-              fontSize: 18.0,
-              fontWeight: FontWeight.w300,
-            ),
-            // task name,
-            // info with number of tasks left for category
-            subtitle1: TextStyle(
-              color: Colors.black,
-              fontSize: 16.0,
-              fontWeight: FontWeight.w400,
-            ),
-            // task name when done
-            subtitle2: TextStyle(
-              color: Colors.grey,
-              fontSize: 16.0,
-              fontWeight: FontWeight.w300,
-              decoration: TextDecoration.lineThrough,
-            ),
-            // dialog title when adding new category
-            bodyText1: TextStyle(
-              color: Colors.grey[700],
-              fontSize: 18.0,
-              fontWeight: FontWeight.w400,
-            ),
-            // info text when adding new category
-            bodyText2: TextStyle(
-              color: Colors.grey,
-              fontSize: 14.0,
-            ),
-            button: TextStyle(
-              color: Colors.white,
-              fontSize: 14.0,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0.8,
-            )),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
