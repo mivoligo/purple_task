573d2d9cc9ef4648b5d55b7957295501c1f64c6b
Michal Predotka <mivoligo@gmail.com>
2020-09-18 20:29:53 +0100

    Use Card widget in settings screen so we can see what's focused

--- lib/ui/widgets/settings/time_format_selector.dart
+++ lib/ui/widgets/settings/time_format_selector.dart
@@ -12,32 +12,38 @@ class TimeFormatSelector extends StatelessWidget {
         Provider.of<SettingsViewModel>(context, listen: false).timeFormats;
     TimeConversion timeConversion = TimeConversion();
 
-    return PopupMenuButton(
-      child: Text(
-        timeConversion.formatDateNow(_settings.getTimeFormat()),
-        style: Theme.of(context)
-            .textTheme
-            .subtitle1
-            .copyWith(color: Theme.of(context).primaryColor),
+    return Card(
+      elevation: 1,
+      child: PopupMenuButton(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            timeConversion.formatDateNow(_settings.getTimeFormat()),
+            style: Theme.of(context)
+                .textTheme
+                .subtitle1
+                .copyWith(color: Theme.of(context).primaryColor),
+          ),
+        ),
+        tooltip: CHANGE_FORMAT,
+        onSelected: (item) => _settings.setTimeFormat(_timeFormatList[item]),
+        itemBuilder: (context) {
+          var list = List<PopupMenuEntry<Object>>();
+          list.add(
+            PopupMenuItem(
+              child: Text(timeConversion.formatDateNow(_timeFormatList[0])),
+              value: 0,
+            ),
+          );
+          list.add(
+            PopupMenuItem(
+              child: Text(timeConversion.formatDateNow(_timeFormatList[1])),
+              value: 1,
+            ),
+          );
+          return list;
+        },
       ),
-      tooltip: CHANGE_FORMAT,
-      onSelected: (item) => _settings.setTimeFormat(_timeFormatList[item]),
-      itemBuilder: (context) {
-        var list = List<PopupMenuEntry<Object>>();
-        list.add(
-          PopupMenuItem(
-            child: Text(timeConversion.formatDateNow(_timeFormatList[0])),
-            value: 0,
-          ),
-        );
-        list.add(
-          PopupMenuItem(
-            child: Text(timeConversion.formatDateNow(_timeFormatList[1])),
-            value: 1,
-          ),
-        );
-        return list;
-      },
     );
   }
 }
