831c9b027759af9df0b44f791a8092a1ab14d836
Michal Predotka <mivoligo@gmail.com>
2020-09-18 17:42:09 +0100

    Use Hive for storing date format

--- lib/ui/view_models/settings_view_model.dart
+++ lib/ui/view_models/settings_view_model.dart
@@ -14,4 +14,16 @@ class SettingsViewModel with ChangeNotifier {
     String timeFormat = box.get(TIME_FORMAT_KEY, defaultValue: H24);
     return timeFormat;
   }
+
+  setDateFormat(String value) {
+    final box = Hive.box(SETTINGS_BOX);
+    box.put(DATE_FORMAT_KEY, value);
+    notifyListeners();
+  }
+
+  getDateFormat() {
+    final box = Hive.box(SETTINGS_BOX);
+    String dateFormat = box.get(DATE_FORMAT_KEY, defaultValue: 'd MMM y');
+    return dateFormat;
+  }
 }
--- lib/ui/widgets/settings/date_format_selector.dart
+++ lib/ui/widgets/settings/date_format_selector.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:purple_task/ui/ui.dart';
 import '../../../globals/globals.dart';
 import '../../../helpers.dart';
 
 class DateFormatSelector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final formatList = [
+    final _settings = Provider.of<SettingsViewModel>(context);
+    final _formatList = [
       'd MMMM y',
       'd MMM y',
       'd/M/y',
@@ -15,43 +18,46 @@ class DateFormatSelector extends StatelessWidget {
     ];
     TimeConversion timeConversion = TimeConversion();
     return PopupMenuButton(
-      child: Text('current format'),
+      child: Text(
+        timeConversion.formatDateNow(_settings.getDateFormat()),
+      ),
       tooltip: CHANGE_FORMAT,
+      onSelected: (item) => _settings.setDateFormat(_formatList[item]),
       itemBuilder: (context) {
         var list = List<PopupMenuEntry<Object>>();
         list.add(
           PopupMenuItem(
-            child: Text(timeConversion.formatDateNow(formatList[0])),
+            child: Text(timeConversion.formatDateNow(_formatList[0])),
             value: 0,
           ),
         );
         list.add(
           PopupMenuItem(
-            child: Text(timeConversion.formatDateNow(formatList[1])),
+            child: Text(timeConversion.formatDateNow(_formatList[1])),
             value: 1,
           ),
         );
         list.add(
           PopupMenuItem(
-            child: Text(timeConversion.formatDateNow(formatList[2])),
+            child: Text(timeConversion.formatDateNow(_formatList[2])),
             value: 2,
           ),
         );
         list.add(
           PopupMenuItem(
-            child: Text(timeConversion.formatDateNow(formatList[3])),
+            child: Text(timeConversion.formatDateNow(_formatList[3])),
             value: 3,
           ),
         );
         list.add(
           PopupMenuItem(
-            child: Text(timeConversion.formatDateNow(formatList[4])),
+            child: Text(timeConversion.formatDateNow(_formatList[4])),
             value: 4,
           ),
         );
         list.add(
           PopupMenuItem(
-            child: Text(timeConversion.formatDateNow(formatList[5])),
+            child: Text(timeConversion.formatDateNow(_formatList[5])),
             value: 5,
           ),
         );
