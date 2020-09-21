4beb728ad61e715b71255bda0131f126c691044b
Michal Predotka <mivoligo@gmail.com>
2020-09-21 17:19:07 +0100

    Use previousFocus() instead of unfocus() to work better with physical keyboard

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
         FocusScopeNode currentFocus = FocusScope.of(context);
         if (!currentFocus.hasPrimaryFocus &&
             currentFocus.focusedChild != null) {
-          currentFocus.focusedChild.unfocus();
+          currentFocus.focusedChild.previousFocus();
         }
       },
       child: MaterialApp(
