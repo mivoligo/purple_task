4f978a584574abfdba56f5bfaca32fafb847e228
Michal Predotka <mivoligo@gmail.com>
2020-10-02 13:19:08 +0100

    Remove unused import

--- lib/ui/widgets/task_item/due_date_selector.dart
+++ lib/ui/widgets/task_item/due_date_selector.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import '../../ui.dart';
 import '../../../db_models/db_models.dart';
