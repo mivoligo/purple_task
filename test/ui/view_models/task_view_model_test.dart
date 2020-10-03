1a3ea08a963976a4e1ac1de3a6f0a376b91cd191
Michal Predotka <mivoligo@gmail.com>
2020-10-03 19:55:40 +0100

    Add tests to check if correct due date is displayed

---
+++ test/ui/view_models/task_view_model_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:purple_task/globals/globals.dart';
+import 'package:purple_task/ui/ui.dart';
+
+void main() {
+  group('Due date', () {
+    test('should return no date', () {
+      final model = TaskViewModel();
+      final dateInMillis = null;
+      final dateFormat = 'yMd';
+      expect(model.displayDueDate(dateInMillis, dateFormat), NO_DATE);
+    });
+    test('should return today', () {
+      final model = TaskViewModel();
+      final todayDate = DateTime.now();
+      final dateInMillis = todayDate.millisecondsSinceEpoch;
+      final dateFormat = 'yMd';
+      print(todayDate.timeZoneOffset);
+      expect(model.displayDueDate(dateInMillis, dateFormat), TODAY);
+    });
+    test('should return tomorrow', () {
+      final model = TaskViewModel();
+      final todayDate = DateTime.now();
+      final tomorrowDate = todayDate.add(Duration(days: 1));
+      final dateInMillis = tomorrowDate.millisecondsSinceEpoch;
+      final dateFormat = 'yMd';
+      expect(model.displayDueDate(dateInMillis, dateFormat), TOMORROW);
+    });
+    test('should return 2021//08/04', () {
+      final model = TaskViewModel();
+      final aDate = DateTime(2021, 8, 4);
+      final dateInMillis = aDate.millisecondsSinceEpoch;
+      final dateFormat = 'y/MM/dd';
+      expect(model.displayDueDate(dateInMillis, dateFormat), '2021/08/04');
+    });
+  });
+}
