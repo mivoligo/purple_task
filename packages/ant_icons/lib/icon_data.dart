0881cd2906f81ace08c61f1bd7bc5e9962f48b9f
Michal Predotka <mivoligo@gmail.com>
2021-03-25 13:14:16 +0000

    Use local package for ant icons

---
+++ packages/ant_icons/lib/icon_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AntIconData extends IconData {
+  const AntIconData(int code)
+      : super(
+          code,
+          fontFamily: 'AntIcons',
+          fontPackage: 'ant_icons',
+        );
+}
