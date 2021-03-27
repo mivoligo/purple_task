e28cec96ba6742da4687f12b0b77a00c98f6d16f
Michal Predotka <mivoligo@gmail.com>
2021-03-27 21:15:58 +0000

    Add functionality to Category cubit

--- lib/categories/bloc/category_cubit.dart
+++ lib/categories/bloc/category_cubit.dart
@@ -1,8 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../db_models/category.dart';
+import '../data/category_repository.dart';
+
 part 'category_state.dart';
 
 class CategoryCubit extends Cubit<CategoryState> {
-  CategoryCubit() : super(CategoryInitial());
+  CategoryCubit({required CategoryRepository categoryRepository})
+      : _categoryRepository = categoryRepository,
+        super(CategoryInitial());
+
+  final CategoryRepository _categoryRepository;
+
+  Future<void> loadCategories() async {
+    emit(CategoryLoading());
+    try {
+      final categories = await _categoryRepository.getCategories();
+      emit(CategoryLoaded(categories));
+    } on Exception {
+      emit(CategoryError());
+    }
+  }
 }
--- lib/categories/bloc/category_state.dart
+++ lib/categories/bloc/category_state.dart
@@ -2,9 +2,21 @@ part of 'category_cubit.dart';
 
 abstract class CategoryState extends Equatable {
   const CategoryState();
-}
-
-class CategoryInitial extends CategoryState {
   @override
   List<Object> get props => [];
 }
+
+class CategoryInitial extends CategoryState {}
+
+class CategoryLoading extends CategoryState {}
+
+class CategoryLoaded extends CategoryState {
+  CategoryLoaded(this.categories);
+
+  final List<Category> categories;
+
+  @override
+  List<Object> get props => [categories];
+}
+
+class CategoryError extends CategoryState {}
