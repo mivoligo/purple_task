d2ad54b0ea04354de48cb35444ecea62f476103a
Michal Predotka <mivoligo@gmail.com>
2021-03-31 17:47:31 +0100

    Center color category screen

--- lib/categories/ui/new_category_color.dart
+++ lib/categories/ui/new_category_color.dart
@@ -23,45 +23,56 @@ class CategoryColor extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(color),
-      body: Card(
-        child: Column(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  const SizedBox(height: 4.0),
-                  Text(
-                    'color', // TODO use global
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                  Spacer(),
-                  const SizedBox(height: 8.0),
-                  Expanded(
-                    child: ColorSelector(
-                      selectedColor: selectedColor,
-                    ),
-                  ),
-                  const SizedBox(height: 4.0),
-                  Row(
+      body: Center(
+        child: SizedBox(
+          width: 600,
+          height: 500,
+          child: Card(
+            margin: const EdgeInsets.all(32),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            elevation: 6,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Column(
                     children: [
-                      const SizedBox(width: 16.0),
-                      SimpleButton(
-                        text: cancel,
-                        onPressed: onCancel,
+                      const SizedBox(height: 4.0),
+                      Text(
+                        'color', // TODO use global
+                        style: Theme.of(context).textTheme.bodyText2,
                       ),
                       Spacer(),
-                      SimpleButton(
-                        text: next,
-                        color: Colors.green,
-                        onPressed: onNext,
+                      const SizedBox(height: 8.0),
+                      Expanded(
+                        child: ColorSelector(
+                          selectedColor: selectedColor,
+                        ),
                       ),
-                      const SizedBox(width: 16.0),
+                      const SizedBox(height: 4.0),
+                      Row(
+                        children: [
+                          const SizedBox(width: 16.0),
+                          SimpleButton(
+                            text: cancel,
+                            onPressed: onCancel,
+                          ),
+                          Spacer(),
+                          SimpleButton(
+                            text: next,
+                            color: Colors.green,
+                            onPressed: onNext,
+                          ),
+                          const SizedBox(width: 16.0),
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
