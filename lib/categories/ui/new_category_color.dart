3e1bb75f5147c20972ac631d2147ca4ef500cc0a
Michal Predotka <mivoligo@gmail.com>
2021-04-01 13:42:49 +0100

    Use gradient as background for new category creator

--- lib/categories/ui/new_category_color.dart
+++ lib/categories/ui/new_category_color.dart
@@ -24,68 +24,83 @@ class CategoryColor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(color),
-      body: Center(
-        child: SizedBox(
-          width: 600,
-          height: 500,
-          child: Card(
-            margin: const EdgeInsets.all(32),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            elevation: 6,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Text(
-                    newCategory,
-                    textAlign: TextAlign.center,
-                    style: CustomStyles().textStyleTitle,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(48.0, 20.0, 32.0, 4.0),
-                  child: Text(
-                    name,
-                    style: CustomStyles().textStyleBigName,
-                  ),
-                ),
-                Expanded(
-                  child: ColorSelector(
-                    selectedColor: selectedColor,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Text(
-                    'Color', // TODO use const string
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SimpleButton(
-                        text: cancel,
-                        onPressed: onCancel,
-                      ),
-                      SimpleButton(
-                        text: next,
-                        color: Colors.green,
-                        onPressed: onNext,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF303030),
+                    Color(color),
+                    Color(color),
+                  ]),
             ),
           ),
-        ),
+          Center(
+            child: SizedBox(
+              width: 600,
+              height: 500,
+              child: Card(
+                margin: const EdgeInsets.all(32),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                elevation: 6,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        newCategory,
+                        textAlign: TextAlign.center,
+                        style: CustomStyles().textStyleTitle,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(48.0, 20.0, 32.0, 4.0),
+                      child: Text(
+                        name,
+                        style: CustomStyles().textStyleBigName,
+                      ),
+                    ),
+                    Expanded(
+                      child: ColorSelector(
+                        selectedColor: selectedColor,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text(
+                        'Color', // TODO use const string
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SimpleButton(
+                            text: cancel,
+                            onPressed: onCancel,
+                          ),
+                          SimpleButton(
+                            text: next,
+                            color: Colors.green,
+                            onPressed: onNext,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/categories/ui/new_category_icon.dart
+++ lib/categories/ui/new_category_icon.dart
@@ -24,84 +24,99 @@ class CategoryIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(color),
-      body: Center(
-        child: SizedBox(
-          width: 600,
-          height: 500,
-          child: Card(
-            margin: const EdgeInsets.all(32),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            elevation: 6,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Text(
-                    newCategory,
-                    textAlign: TextAlign.center,
-                    style: CustomStyles().textStyleTitle,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 15.0, 32.0, 4.0),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 2),
-                        child: Icon(
-                          IconData(
-                            selectedIcon,
-                            fontFamily: 'AntIcons',
-                            fontPackage: 'ant_icons',
-                          ),
-                          color: Color(color),
-                          size: 38,
-                        ),
-                      ),
-                      Text(
-                        name,
-                        style: CustomStyles().textStyleBigName,
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: IconSelector(
-                    selectedIcon: selectedIcon,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Text(
-                    'Icon', // TODO use const string
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SimpleButton(
-                        text: cancel,
-                        onPressed: onCancel,
-                      ),
-                      SimpleButton(
-                        text: next,
-                        color: Colors.green,
-                        onPressed: onNext,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF303030),
+                    Color(color),
+                    Color(color),
+                  ]),
             ),
           ),
-        ),
+          Center(
+            child: SizedBox(
+              width: 600,
+              height: 500,
+              child: Card(
+                margin: const EdgeInsets.all(32),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                elevation: 6,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        newCategory,
+                        textAlign: TextAlign.center,
+                        style: CustomStyles().textStyleTitle,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8.0, 15.0, 32.0, 4.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 2),
+                            child: Icon(
+                              IconData(
+                                selectedIcon,
+                                fontFamily: 'AntIcons',
+                                fontPackage: 'ant_icons',
+                              ),
+                              color: Color(color),
+                              size: 38,
+                            ),
+                          ),
+                          Text(
+                            name,
+                            style: CustomStyles().textStyleBigName,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: IconSelector(
+                        selectedIcon: selectedIcon,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text(
+                        'Icon', // TODO use const string
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SimpleButton(
+                            text: cancel,
+                            onPressed: onCancel,
+                          ),
+                          SimpleButton(
+                            text: next,
+                            color: Colors.green,
+                            onPressed: onNext,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
