97857f8ba8b08fb5e38fc80c871ba3a5e634c55b
Michal Predotka <mivoligo@gmail.com>
2024-12-05 14:48:12 +0100

    Remove unused files

--- lib/ui/screens/main_screen/widgets/widgets.dart
+++ lib/ui/screens/main_screen/widgets/widgets.dart
@@ -1,6 +1,4 @@
 export 'add_category_button.dart';
 export 'animated_background.dart';
 export 'greetings.dart';
-export 'narrow_layout/narrow_layout_og.dart';
 export 'uncategorized_tasks.dart';
-export 'wide_layout/wide_layout_og.dart';
