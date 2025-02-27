7f33b0c9af792eaf69cd40a71ff7a9152d190250
Michal Predotka <mivoligo@gmail.com>
2025-02-27 10:47:11 +0000

    Adjust due date color

--- lib/features/todos/views/due_date_indicator.dart
+++ lib/features/todos/views/due_date_indicator.dart
@@ -1,6 +1,7 @@
 import 'package:ant_icons/ant_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:purple_task/core/constants/custom_styles.dart';
 
 import 'package:purple_task/core/helpers.dart';
 import 'package:purple_task/features/settings/controllers/settings_controller.dart';
@@ -16,6 +17,7 @@ class DueDateIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
     return Consumer(
       builder: (context, ref, _) {
         final dateFormatSetting =
@@ -23,9 +25,12 @@ class DueDateIndicator extends StatelessWidget {
 
         return Row(
           children: [
-            const Icon(
+            Icon(
               AntIcons.calendarOutline,
               size: 14,
+              color: task.isDone
+                  ? colorScheme.onSurface.withAlpha(180)
+                  : colorScheme.onSurface,
             ),
             const SizedBox(width: 2),
             Text(
@@ -33,7 +38,11 @@ class DueDateIndicator extends StatelessWidget {
                 task.dueDate,
                 dateFormatSetting,
               ),
-              style: Theme.of(context).textTheme.bodySmall,
+              style: CustomStyle.textStyleLabelSmall.copyWith(
+                color: task.isDone
+                    ? colorScheme.onSurface.withAlpha(180)
+                    : colorScheme.onSurface,
+              ),
             ),
           ],
         );
