06c8d763eb594f05e5737ca1b09fa319672b94d2
Michal Predotka <mivoligo@gmail.com>
2025-02-27 10:53:42 +0000

    Refactor time and date selector widgets

--- lib/features/settings/views/widgets/date_format_selector.dart
+++ lib/features/settings/views/widgets/date_format_selector.dart
@@ -4,43 +4,39 @@ import 'package:purple_task/core/constants/time_and_date_formats.dart';
 import 'package:purple_task/core/helpers.dart';
 import 'package:purple_task/features/settings/controllers/settings_controller.dart';
 
-class DateFormatSelector extends StatelessWidget {
+class DateFormatSelector extends ConsumerWidget {
   const DateFormatSelector({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, _) {
-        final settingsState = ref.watch(settingsNotifierProvider);
-        final controller = ref.watch(settingsNotifierProvider.notifier);
-        return MenuAnchor(
-          menuChildren: [
-            ...dateFormats.map(
-              (dateFormat) => MenuItemButton(
-                onPressed: () {
-                  controller.setDateFormat(dateFormat: dateFormat);
-                },
-                child: Text(
-                  TimeConverter.formatDateNow(dateFormat),
-                  style: settingsState.value?.dateFormat == dateFormat
-                      ? const TextStyle(fontWeight: FontWeight.bold)
-                      : null,
-                ),
-              ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final settingsState = ref.watch(settingsNotifierProvider);
+    final controller = ref.watch(settingsNotifierProvider.notifier);
+    return MenuAnchor(
+      menuChildren: [
+        ...dateFormats.map(
+          (dateFormat) => MenuItemButton(
+            onPressed: () {
+              controller.setDateFormat(dateFormat: dateFormat);
+            },
+            child: Text(
+              TimeConverter.formatDateNow(dateFormat),
+              style: settingsState.value?.dateFormat == dateFormat
+                  ? const TextStyle(fontWeight: FontWeight.bold)
+                  : null,
             ),
-          ],
-          builder: (context, controller, child) {
-            return OutlinedButton(
-              onPressed: () {
-                controller.isOpen ? controller.close() : controller.open();
-              },
-              child: Text(
-                TimeConverter.formatDateNow(
-                  settingsState.value?.dateFormat ?? 'd MMM y',
-                ),
-              ),
-            );
+          ),
+        ),
+      ],
+      builder: (context, controller, child) {
+        return OutlinedButton(
+          onPressed: () {
+            controller.isOpen ? controller.close() : controller.open();
           },
+          child: Text(
+            TimeConverter.formatDateNow(
+              settingsState.value?.dateFormat ?? 'd MMM y',
+            ),
+          ),
         );
       },
     );
--- lib/features/settings/views/widgets/time_format_selector.dart
+++ lib/features/settings/views/widgets/time_format_selector.dart
@@ -4,43 +4,39 @@ import 'package:purple_task/core/constants/time_and_date_formats.dart';
 import 'package:purple_task/core/helpers.dart';
 import 'package:purple_task/features/settings/controllers/settings_controller.dart';
 
-class TimeFormatSelector extends StatelessWidget {
+class TimeFormatSelector extends ConsumerWidget {
   const TimeFormatSelector({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, _) {
-        final settingsState = ref.watch(settingsNotifierProvider);
-        final controller = ref.watch(settingsNotifierProvider.notifier);
-        return MenuAnchor(
-          menuChildren: [
-            ...timeFormats.map(
-              (timeFormat) => MenuItemButton(
-                onPressed: () {
-                  controller.setTimeFormat(timeFormat: timeFormat);
-                },
-                child: Text(
-                  TimeConverter.formatDateNow(timeFormat),
-                  style: settingsState.value?.timeFormat == timeFormat
-                      ? const TextStyle(fontWeight: FontWeight.bold)
-                      : null,
-                ),
-              ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final settingsState = ref.watch(settingsNotifierProvider);
+    final controller = ref.watch(settingsNotifierProvider.notifier);
+    return MenuAnchor(
+      menuChildren: [
+        ...timeFormats.map(
+          (timeFormat) => MenuItemButton(
+            onPressed: () {
+              controller.setTimeFormat(timeFormat: timeFormat);
+            },
+            child: Text(
+              TimeConverter.formatDateNow(timeFormat),
+              style: settingsState.value?.timeFormat == timeFormat
+                  ? const TextStyle(fontWeight: FontWeight.bold)
+                  : null,
             ),
-          ],
-          builder: (context, controller, child) {
-            return OutlinedButton(
-              onPressed: () {
-                controller.isOpen ? controller.close() : controller.open();
-              },
-              child: Text(
-                TimeConverter.formatDateNow(
-                  settingsState.value?.timeFormat ?? 'Hm',
-                ),
-              ),
-            );
+          ),
+        ),
+      ],
+      builder: (context, controller, child) {
+        return OutlinedButton(
+          onPressed: () {
+            controller.isOpen ? controller.close() : controller.open();
           },
+          child: Text(
+            TimeConverter.formatDateNow(
+              settingsState.value?.timeFormat ?? 'Hm',
+            ),
+          ),
         );
       },
     );
