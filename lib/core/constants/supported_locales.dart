d59fb59cc5e88a4de6026d360d3d528c16a8c820
Mylo Fawcett <mylo.fawcett@gmail.com>
2025-03-14 13:16:54 +0100

    Add french translations

--- lib/core/constants/supported_locales.dart
+++ lib/core/constants/supported_locales.dart
@@ -3,11 +3,13 @@ import 'dart:ui';
 class SupportedLocales {
   static const en = Locale('en');
   static const pl = Locale('pl');
+  static const fr = Locale('fr');
 
   static String localeToString(Locale locale) {
     return switch (locale) {
       en => 'English',
       pl => 'Polski',
+      fr => 'Français',
       _ => '',
     };
   }
