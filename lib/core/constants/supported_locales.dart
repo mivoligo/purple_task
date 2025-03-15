41357425a58940b2d7f8ef37fba49df71c049c25
Dustin Bürstinghaus <dustin.buerstinghaus@gmx.de>
2025-03-15 16:49:23 +0100

    add german translation

--- lib/core/constants/supported_locales.dart
+++ lib/core/constants/supported_locales.dart
@@ -4,12 +4,14 @@ class SupportedLocales {
   static const en = Locale('en');
   static const pl = Locale('pl');
   static const fr = Locale('fr');
+  static const de = Locale('de');
 
   static String localeToString(Locale locale) {
     return switch (locale) {
       en => 'English',
       pl => 'Polski',
       fr => 'Français',
+      de => 'Deutsch',
       _ => '',
     };
   }
